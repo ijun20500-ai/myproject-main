d0a8b89edf24d16017e3853c0a12ac50950d212f
ijun20500-ai <ijun20500@gmail.com>
2026-07-28 16:41:08 +0900

    first commit

---
+++ pybo/models.py
@@ -0,0 +1,62 @@
+from pybo import db
+from sqlalchemy import Table
+
+# 중간 테이블 정의
+question_voter = Table(
+    'question_voter',
+    db.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
+answer_voter = Table(
+    'answer_voter',
+    db.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    # 글쓴이 외래키 및 관계 설정 추가 (기존 데이터 고려 nullable=True 우선 허용)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('question_set'))
+     # 추천인 (다대다)
+    voter = db.relationship('User', secondary=question_voter,
+                            backref=db.backref('question_voter_set', lazy='dynamic'))
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    questions_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('answer_set', cascade='all, delete-orphan'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    # 글쓴이 외래키 및 관계 설정 추가
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('answer_set'))
+    voter = db.relationship('User', secondary=answer_voter,
+                            backref=db.backref('answer_voter_set', lazy='dynamic'))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('comment_set'))
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+    modify_date = db.Column(db.DateTime())
+    # 질문 테이블 및 답변 테이블과의 다대일(N:1) 관계 외래키 매핑
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), nullable=True)
+    question = db.relationship('Question', backref=db.backref('comment_set'))
+    answer_id = db.Column(db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), nullable=True)
+    answer = db.relationship('Answer', backref=db.backref('comment_set'))
+
---
+++ pybo/views/auth_views.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, render_template, request, session, g, url_for, redirect, flash
+from werkzeug.security import check_password_hash, generate_password_hash
+from pybo import db
+from pybo.forms import UserCreateForm, UserLoginForm
+from pybo.models import User
+import functools # 함수 도구 모듈 임포트
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+#회원등록
+@bp.route('/signup/', methods=('GET', 'POST'))
+def signup():
+    form = UserCreateForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            user = User(username=form.username.data,
+                        password=generate_password_hash(form.password1.data),
+                        email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        else:
+            flash('이미 존재하는 사용자입니다.')
+    return render_template('auth/signup.html', form=form)
+
+@bp.route('/login/', methods=('GET', 'POST'))
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/login.html', form=form)
+
+# 사용자 로그인 정보를 g.user변수에 저장
+# @bp.before_app_request :  어떤 라우트 함수가 실행되더라도 그전에 세션을 검사함
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(*args, **kwargs)
+    return wrapped_view
